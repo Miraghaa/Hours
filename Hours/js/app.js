9dd58604ebd1265c51e82cf29c37dcd84eae1dfb
Miraghaa <miraghaea@code.edu.az>
2023-06-02 04:00:48 +0400

    updated

--- Hours/js/app.js
+++ Hours/js/app.js
@@ -3,21 +3,21 @@ let date = new Date();
 let x = date.getHours();
 
 if(x>22 || x<5 || x==0){
-    alert('gecedi') 
+    alert('geceler bulaq basi') 
     document.body.style.backgroundImage = 'url(https://wallpaperaccess.com/full/386677.jpg)'
     document.getElementById('miri').style.height ='0px'
 }else if(x>4 && x<11){
-    alert('seher') 
+    alert('seher tezden acilanda pencere') 
     document.body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fHww&w=1000&q=80)'
     document.getElementById('miri').style.height ='0px'
 }
 else if(x>10 && x<18){
-    alert('gunorta') 
+    alert('Gunorta havada isti olajax deyifler') 
     document.body.style.backgroundImage = 'url(https://media.istockphoto.com/id/531853066/vector/sun-character-with-funny-facial-expressions-in-glossy-for-summer.jpg?s=1024x1024&w=is&k=20&c=THTzkGSKHP9jhr2psmu8N6P40uJlvCV4CoDbFlpzBEc=)'
     document.getElementById('miri').style.height ='0px'
 }
 else if(x>17 && x<=22){
-    alert('axsam') 
+    alert('Axsamdi cix get evive') 
     document.body.style.backgroundImage = 'url(https://media.istockphoto.com/id/1172427455/photo/beautiful-sunset-over-the-tropical-sea.jpg?s=612x612&w=0&k=20&c=i3R3cbE94hdu6PRWT7cQBStY_wknVzl2pFCjQppzTBg=)'
     document.getElementById('miri').style.height ='0px'
 }
